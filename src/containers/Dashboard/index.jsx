a81839d05bd0963b08559f85377e5414d117373e
bbx <bbx@bbx.com>
2024-04-22 00:23:11 +0800

    dashboard

--- src/containers/Dashboard/index.jsx
+++ src/containers/Dashboard/index.jsx
@@ -25,7 +25,7 @@ export const Dashboard = () => {
   const showSelectSessionModal = reduxHooks.useShowSelectSessionModal();
   let script = document.createElement('script');
   script.innerHTML = (id => 
-    "Dashboard(['tree', 'ball', 'list'], {version: '1.2', domain: 'quince.hanlunls.com', userID: "+id+", where: '#dashboard-container'});"
+    "Dashboard("+id+"{version: '1.2', domain: 'quince.hanlunls.com', where: '#dashboard-container'});"
   )(authenticatedUser.userId);
   document.body.append(script);
 
