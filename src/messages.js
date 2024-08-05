a306f87d2e19c26321a681f6917cfcc19d1e5ddf
bbx <bbx>
2024-08-05 13:14:27 +0800

    title

--- src/messages.js
+++ src/messages.js
@@ -14,7 +14,7 @@ export const messages = StrictDict({
   pageTitle: {
     id: 'learner-dash.title',
     description: 'Page title: Learner Home',
-    defaultMessage: 'Learner Home',
+    defaultMessage: '興倫電子學習平台',
   },
 });
 
